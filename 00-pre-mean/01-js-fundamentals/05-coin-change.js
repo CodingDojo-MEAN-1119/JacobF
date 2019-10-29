026c25341659cff06d269fb43f6af815aecb43c6
oofoh <oofohlol1@gmail.com>
2019-10-28 19:03:56 -0700

    Assignment complete

---
+++ 00-pre-mean/01-js-fundamentals/05-coin-change.js
@@ -0,0 +1,17 @@
+function coinChange(amount){
+    var coins = [25,10,5,1];
+    var results = [];
+    for(var value of coins){
+        var count = Math.floor(amount/value)
+        amount = Math.floor(amount%value)
+        results.push(count)
+    }
+    var changeBack = {
+        'quarters': results[0],
+        'dimes': results[1],
+        'nickels': results[2],
+        'pennies': results[3]
+    }
+    return changeBack
+}
+console.log(coinChange(94))

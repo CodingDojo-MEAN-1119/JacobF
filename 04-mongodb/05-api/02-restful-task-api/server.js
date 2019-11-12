72a3991d1d54d2d7a8f8368649b4737b048cff4d
oofoh <oofohlol1@gmail.com>
2019-11-12 15:27:52 -0800

    Assignment complete

---
+++ 04-mongodb/05-api/02-restful-task-api/server.js
@@ -0,0 +1,12 @@
+// EXPRESS SETUP //
+const express = require('express');
+const app = express();
+const server = app.listen(8000);
+// EXPRESS MIDDLEWARE + SERVE STATIC FILES //
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// SERVER-SIDE SCRIPT + MONGOOSE //
+require('./server/config/mongoose.js')
+require('./server/config/routes.js')(app)

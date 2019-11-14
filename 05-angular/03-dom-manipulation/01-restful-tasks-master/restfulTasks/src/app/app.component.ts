4b07e0f37fe47b18cec00970e307261f48ec03df
oofoh <oofohlol1@gmail.com>
2019-11-13 20:19:52 -0800

    Assignment complete

--- 05-angular/03-dom-manipulation/01-restful-tasks-master/restfulTasks/src/app/app.component.ts
+++ 05-angular/03-dom-manipulation/01-restful-tasks-master/restfulTasks/src/app/app.component.ts
@@ -9,14 +9,18 @@ import { Task } from './models/task';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'restfulTaskAPI';
+  title = 'restfulTasks';
   tasks: Task[] = [];
   task: Task;
+  newTask: any;
+  editedTask: Task;
+  editForm: boolean;
 
   constructor(private httpService: HttpService) {}
 
-
   ngOnInit() {
+    this.newTask = {title: '', description: ''};
+    this.editedTask = {_id: '', title: '', description: '', completed: false};
   }
 
   getTasksFromService() {
@@ -37,6 +41,54 @@ export class AppComponent implements OnInit {
 
   hideAllTasks() {
     this.tasks = [];
+    this.task = null;
+    this.editForm = false;
+  }
+
+  hideDetails() {
+    this.task = null;
+    this.editForm = false;
+  }
+
+  createTask() {
+    this.httpService.createTask(this.newTask)
+      .subscribe(newTask => {
+        console.log('creating new task', newTask);
+        if (this.tasks.length > 0) {
+          this.tasks = [... this.tasks, newTask];
+        } else {
+          this.tasks = [];
+        }
+      });
+    this.newTask = {title: '', description: ''};
+  }
+
+  showEditForm() {
+    this.editForm = true;
+    this.editedTask = this.task;
+  }
+
+  closeEditForm() {
+    this.editForm = false;
+  }
+
+  updateTask(taskId: string) {
+    this.httpService.updateTask(taskId, this.editedTask)
+      .subscribe(thisTask => {
+        console.log('updated this task', thisTask);
+        this.task = thisTask;
+        this.editForm = false;
+      });
+  }
+
+  deleteTask(taskId: string) {
+    this.httpService.deleteTask(taskId)
+      .subscribe(thisTask => {
+        console.log('deleted this task', thisTask);
+        this.tasks = this.tasks.filter(
+          currentTask => currentTask._id !== thisTask._id,
+        );
+      });
     this.task = null;
   }
 
--- 05-angular/03-dom-manipulation/01-restful-tasks-master/restfulTasks/src/app/services/http.service.ts
+++ 05-angular/03-dom-manipulation/01-restful-tasks-master/restfulTasks/src/app/services/http.service.ts
@@ -20,5 +20,17 @@ export class HttpService {
     return this.http.get<Task>(`/tasks/${taskId}`);
   }
 
+  createTask(newTask): Observable<Task> {
+    return this.http.post<Task>('/tasks', newTask);
+  }
+
+  updateTask(taskId: string, editedTask: Task): Observable<Task> {
+    return this.http.put<Task>(`/tasks/${taskId}`, editedTask);
+  }
+
+  deleteTask(taskId: string): Observable<Task> {
+    return this.http.delete<Task>(`/tasks/${taskId}`);
+  }
+
 }
 
